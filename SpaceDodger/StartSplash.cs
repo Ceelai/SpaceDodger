aaae32ac4e08f2ca1e9cc33bd84ee942c1395750
Calvin Lai <laica@sheridancollege.ca>
2016-04-13 11:50:37 -0400

    Fixed Splash Screen Bug.

---
+++ SpaceDodger/StartSplash.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceDodger
+{
+    public partial class StartSplash : Form
+    {
+        public StartSplash()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+
+        int sec = 0;
+        Form1 forma = new Form1();
+        private void timer1_tick(object sender, EventArgs e)
+        {
+            sec = sec + 1;
+            if (sec == 5)
+            {
+                forma.Visible = true;
+                this.Hide();
+
+            }
+        }
+    }
+}
+
